64c39d1a54f04f6b0954d2e71cbf4bff72a02ddd
analogic <info@analogic.cz>
2016-08-05 15:48:16 +0200

    Update license agreement

--- Lescript.php
+++ Lescript.php
@@ -6,7 +6,7 @@ class Lescript
 {
     public $ca = 'https://acme-v01.api.letsencrypt.org';
     // public $ca = 'https://acme-staging.api.letsencrypt.org'; // testing
-    public $license = 'https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf';
+    public $license = 'https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf';
     public $countryCode = 'CZ';
     public $state = "Czech Republic";
 
