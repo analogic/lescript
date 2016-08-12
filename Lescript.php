6c1fb8d43543ac9688f42538e53cf809a02ceebd
Andreas Gohr <andi@splitbrain.org>
2016-08-12 19:14:31 +0200

    Make HTTP client exchangable
    This introduces an Interface that defines what a suitable HTTPClient
    needs to provide and makes it possible to pass an alternative
    implementation in the Lescript constructor.

--- Lescript.php
+++ Lescript.php
@@ -18,12 +18,16 @@ class Lescript
     private $client;
     private $accountKeyPath;
 
-    public function __construct($certificatesDir, $webRootDir, $logger = null)
+    public function __construct($certificatesDir, $webRootDir, $logger = null, ClientInterface $client = null)
     {
         $this->certificatesDir = $certificatesDir;
         $this->webRootDir = $webRootDir;
         $this->logger = $logger;
-        $this->client = new Client($this->ca);
+        if($client !== null) {
+            $this->client = $client;
+        } else {
+            $this->client = new Client($this->ca);
+        }
         $this->accountKeyPath = $certificatesDir . '/_account/private.pem';
     }
 
@@ -379,7 +383,66 @@ keyUsage = nonRepudiation, digitalSignature, keyEncipherment');
     }
 }
 
-class Client
+interface ClientInterface
+{
+    /**
+     * Constructor
+     *
+     * @param string $base the ACME API base all relative requests are sent to
+     */
+    public function __construct($base);
+
+    /**
+     * Send a POST request
+     *
+     * @param string $url URL to post to
+     * @param array $data fields to sent via post
+     * @return array|string the parsed JSON response, raw response on error
+     */
+    public function post($url, $data);
+
+    /**
+     * @param string $url URL to request via get
+     * @return array|string the parsed JSON response, raw response on error
+     */
+    public function get($url);
+
+    /**
+     * Returns the Replay-Nonce header of the last request
+     *
+     * if no request has been made, yet. A GET on $base/directory is done and the
+     * resulting nonce returned
+     *
+     * @return mixed
+     */
+    public function getLastNonce();
+
+    /**
+     * Return the Location header of the last request
+     *
+     * returns null if last request had no location header
+     *
+     * @return string|null
+     */
+    public function getLastLocation();
+
+    /**
+     * Return the HTTP status code of the last request
+     *
+     * @return int
+     */
+    public function getLastCode();
+
+    /**
+     * Get all Link headers of the last request
+     *
+     * @return string[]
+     */
+    public function getLastLinks();
+}
+
+
+class Client implements ClientInterface
 {
     private $lastCode;
     private $lastHeader;
